2665cb7039e411895297dd9704fd7cd5fd9ea740
boyfromthewell <86250281+boyfromthewell@users.noreply.github.com>
2022-01-26 21:52:47 +0900

    Delete BOJ_5622.py

--- BOJ_5622.py
+++
@@ -1,9 +0,0 @@
-data=['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
-
-n=input()
-result=0
-for i in range(len(n)):
-  for j in data:
-    if n[i] in j:
-      result+=data.index(j)+3
-print(result)
