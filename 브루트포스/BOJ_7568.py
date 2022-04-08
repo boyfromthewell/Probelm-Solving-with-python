1a82f1367624337c8fd52c9ab8d7ee2a91fb4397
boyfromthewell <86250281+boyfromthewell@users.noreply.github.com>
2022-04-08 17:50:02 +0900

    Add files via upload

---
+++ 브루트포스/BOJ_7568.py
@@ -0,0 +1,14 @@
+n=int(input())
+data=[]
+
+for _ in range(n):
+  weight,height=map(int,input().split())
+  data.append((weight,height))
+
+for i in data:
+  rank=1
+  for j in data:
+    if i[0]<j[0] and i[1]<j[1]:
+      rank+=1
+  print(rank, end=" ")
+  
